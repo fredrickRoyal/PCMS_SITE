97d9caac3bc6579626e243628118c421f1afa524
benjamin okiza <51708648+benjamin-markintosh@users.noreply.github.com>
2025-08-08 10:21:06 +0300

    Enhance Hero component layout and update partnership requirement descriptions for clarity

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -41,7 +41,7 @@ export function Hero() {
         </div>
       </div>
       <div className="hidden md:flex w-full z-10 items-center justify-end">
-        <div className="space-y-4">
+        <div className="space-y-4 max-w-4xl">
           <Link
             href="/partner-requirements"
             className="px-6 py-3 bg-white/80 flex gap-4 text-black border border-white/50 rounded-md font-medium hover:bg-white/100"
@@ -50,9 +50,9 @@ export function Hero() {
               <h3 className="text-lg font-normal">
                 Requirements for partnership
               </h3>
-              <p className="text-lg font-light">
-                The Government of Uganda Refugee Response Monitoring System
-                (GoU-RRMS)
+              <p className="text-lg font-light line-clamp-1">
+                As part of its mandate, the Department is responsible for
+                registering all organisations engaged…
               </p>
             </div>
             <LucideChevronRight />
@@ -65,9 +65,10 @@ export function Hero() {
               <h3 className="text-lg font-normal">
                 The Comprehensive Refugee Response Framework (CRRF) in Uganda
               </h3>
-              <p className="text-lg font-light">
-                The Government of Uganda Refugee Response Monitoring System
-                (GoU-RRMS)
+              <p className="text-lg font-light line-clamp-1">
+                Uganda sets the global standard by redefining refugee response,
+                shifting from short-term relief to lasting development and
+                community cohesion.
               </p>
             </div>
             <LucideChevronRight />
@@ -80,9 +81,9 @@ export function Hero() {
               <h3 className="text-lg font-normal">
                 Support government coordination now
               </h3>
-              <p className="text-lg font-light">
-                The Government of Uganda Refugee Response Monitoring System
-                (GoU-RRMS)
+              <p className="text-lg font-light line-clamp-1">
+                The increasing number of arrivals, coupled with limited
+                resources, has strained Uganda’s refugee response system
               </p>
             </div>
             <LucideChevronRight />
